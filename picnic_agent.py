c1c8f84d137aa80f459a2b48bb9514d783911a5f
Jensssen <soeren.erichsen@gmx.de>
2025-01-19 17:37:18 +0100

    combine both agent scripts to one

--- picnic_agent.py
+++ picnic_agent.py
@@ -2,18 +2,16 @@ import asyncio
 import itertools
 import os
 import traceback
-from typing import Generator
 from pprint import pprint
+from typing import Generator
 
 import pyaudio
 from dotenv import load_dotenv
 from google import genai
 from google.cloud import texttospeech
 
-from picnic_tools import (search_for_products, add_product_to_cart, remove_product_from_cart, search_for_recipes,
-                          add_recipe_to_cart, search_for_cheaper_product_alternative, replace_existing_product,
-                          get_all_current_products_in_cart)
-from picnic_tools import tools
+from tools.picnic_tools import handle_picnic_tool_operations
+from tools.tool_descriptions import tools
 
 load_dotenv()
 
@@ -24,25 +22,18 @@ RECEIVE_SAMPLE_RATE = 24000
 CHUNK_SIZE = 1024
 MODEL = "models/gemini-2.0-flash-exp"
 pya = pyaudio.PyAudio()
+RESPONSE_MODEL = "TEXT"  # or "AUDIO" <- AUDIO is not well-supported yet...
 
 client = genai.Client(api_key=os.environ.get("GEMINI_API_KEY"), http_options={"api_version": "v1alpha"})
 text_to_speach_client = texttospeech.TextToSpeechClient()
 
-CONFIG = {"generation_config": {"response_modalities": ["TEXT"], "temperature": 0},
+CONFIG = {"generation_config": {"response_modalities": [RESPONSE_MODEL], "temperature": 0},
           "system_instruction": "You are a shopping assistant called Picnic Pal 3000 for the online grocery store "
-                                "Picnic. DO NOT HALLUCINATE!",
+                                "Picnic.",
           "tools": [tools]}
 streaming_config = texttospeech.StreamingSynthesizeConfig(
     voice=texttospeech.VoiceSelectionParams(name="en-US-Journey-D", language_code="en-US"))
 config_request = texttospeech.StreamingSynthesizeRequest(streaming_config=streaming_config)
-
-TOOLS = {
-    "search_for_products": {"function": search_for_products, "args": ["search_query"]},
-    "search_for_recipes": {"function": search_for_recipes, "args": ["search_query"]},
-    "add_recipe_to_cart": {"function": add_recipe_to_cart, "args": ["recipe_id"]},
-    "search_for_cheaper_product_alternative": {"function": search_for_cheaper_product_alternative,
-                                               "args": ["product_name"]},
-}
 
 
 class AudioLoop:
@@ -100,8 +91,9 @@ class AudioLoop:
                     continue
                 if text := response.text:
                     print(text, end="")
-                    data = await self.stream_text_to_speech(text)
-                    self.audio_in_queue.put_nowait(data)
+                    if RESPONSE_MODEL == "TEXT":
+                        data = await self.stream_text_to_speech(text)
+                        self.audio_in_queue.put_nowait(data)
                     continue
                 if _ := response.tool_call:
                     function_calls = response.tool_call.function_calls
@@ -110,62 +102,21 @@ class AudioLoop:
                         args = function_call.args
                         call_id = function_call.id
                         try:
-                            if name == "add_product_to_cart":
-                                product_id = args["product_id"]
-                                try:
-                                    count = args["count"]
-                                except KeyError:
-                                    count = 1
-                                result = add_product_to_cart(str(product_id), int(count))
-                                function_responses.append({
-                                    "name": name,
-                                    "response": {"result": result},
-                                    "id": call_id
-                                })
-                            if name == "get_all_current_products_in_cart":
-                                result = get_all_current_products_in_cart()
-                                function_responses.append({
-                                    "name": name,
-                                    "response": {"result": result},
-                                    "id": call_id
-                                })
-                            if name == "remove_product_from_cart":
-                                product_id = args["product_id"]
-                                try:
-                                    count = args["count"]
-                                except KeyError:
-                                    count = 1
-                                result = remove_product_from_cart(str(product_id), int(count))
-                                function_responses.append({
-                                    "name": name,
-                                    "response": {"result": result},
-                                    "id": call_id
-                                })
-                            if name == "replace_existing_product":
-                                result = replace_existing_product(str(args["old_product_id"]),
-                                                                  str(args["new_product_id"]))
-                                function_responses.append({
-                                    "name": name,
-                                    "response": result,
-                                    "id": call_id
-                                })
-                        except Exception as e:
-                            print(f"Error executing {name} function with error: {e}")
-
-                        try:
-                            tool = TOOLS[name]
-                            result = tool["function"](args[tool["args"][0]])
-                            function_responses.append({
-                                "name": name,
-                                "response": result,
-                                "id": call_id
-                            })
+                            response = handle_picnic_tool_operations(name, args, call_id)
+                            function_responses.append(response)
                         except Exception as e:
                             print(f"Error executing {name} function with error: {e}")
                     pprint(function_responses, width=180, indent=2, compact=False)
                     # Send function result back to Gemini
                     await self.session.send(function_responses)
                     continue
+            if RESPONSE_MODEL == "AUDIO":
+                # If you interrupt the model, it sends a turn_complete.
+                # For interruptions to work, we need to stop playback.
+                # So empty out the audio queue because it may have loaded
+                # much more audio than has played yet.
+                while not self.audio_in_queue.empty():
+                    self.audio_in_queue.get_nowait()
 
     async def play_audio(self) -> None:
         stream = await asyncio.to_thread(
